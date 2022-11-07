d9843fabac4b0db9d5fdfabaec023f896033c0f1
Richard Mwewa <74001397+rly0nheart@users.noreply.github.com>
2022-11-07 21:44:16 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open('README.md', 'r', encoding='utf-8') as file:
+    long_description = file.read()
+
+setuptools.setup(
+    name='youtube-comment-scraper',
+    version='2022.1.4.4',
+    author='Richard Mwewa',
+    author_email='rly0nheart@duck.com',
+    packages=['youtube_comment_scraper'],
+    description='YouTube Comment Scraper',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/rly0nheart/youtube-comment-scraper',
+    license='GNU General Public License v3 (GPLv3)',
+    install_requires=['requests', 'youtube-comment-downloader'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Information Technology',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',  
+        'Operating System :: OS Independent',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3'
+        ],
+    entry_points={
+        'console_scripts': [
+            'youtube_comment_scraper=youtube_comment_scraper.main:main',
+        ]
+    },
+)
