455fe8a3186bceaff61006bef2ae7fcf438fd77a
Richard Mwewa <74001397+rly0nheart@users.noreply.github.com>
2022-11-07 23:14:35 +0200

    Update main.py

--- youtube_comment_scraper/main.py
+++ youtube_comment_scraper/main.py
@@ -12,7 +12,7 @@ def main():
     _parser = create_parser()
     args = _parser.parse_args()
     try:
-        YouTubeCommentScraper().find_multiple_authors(args.videos)
+        YouTubeCommentScraper().find_multiple_users(args.videos)
     except KeyboardInterrupt:
         print("[x] Process interrupted with Ctrl+C.")
 
