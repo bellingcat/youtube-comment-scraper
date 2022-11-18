8ec8e2d64c48837d985a6ff1594967d9ccf792a0
Richard Mwewa <74001397+rly0nheart@users.noreply.github.com>
2022-11-18 02:24:41 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as file:
 
 setuptools.setup(
     name='youtube-comment-scraper',
-    version='2022.1.4.4',
+    version='2022.1.2.0',
     author='Richard Mwewa',
     author_email='rly0nheart@duck.com',
     packages=['youtube_comment_scraper'],
